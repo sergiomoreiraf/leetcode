877c972310fb84d375786149fd793ecd3321e562
Sergio Moreira <sergio.moreira@serpro.gov.br>
2025-03-05 15:04:26 -0300

    problem 217

--- problems/217.ContainsDuplicate/main.go
+++ problems/217.ContainsDuplicate/main.go
@@ -1,5 +1,12 @@
 package problems
 
-// func containsDuplicate(nums []int) bool {
-
-// }
+func containsDuplicate(nums []int) bool {
+	set := make(map[int]bool)
+	for _, v := range nums {
+		if _, ok := set[v]; ok {
+			return true
+		}
+		set[v] = true
+	}
+	return false
+}
--- problems/217.ContainsDuplicate/main_test.go
+++ problems/217.ContainsDuplicate/main_test.go
@@ -1,5 +1,15 @@
 package problems
 
-// func Test_containsDuplicate(t *testing.T) {
-// 	containsDuplicate()
-// }
+import "testing"
+
+func TestContainsDuplicate(t *testing.T) {
+	if x := containsDuplicate([]int{1, 2, 3, 1}); x != true {
+		t.Error("Error")
+	}
+	if x := containsDuplicate([]int{1, 2, 3, 4}); x != false {
+		t.Error("Error")
+	}
+	if x := containsDuplicate([]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}); x != true {
+		t.Error("Error")
+	}
+}
